ca9c5ea3f6da7aefc5e4871d10b15c7a30f01406
Krystian Budulski <krystian.budulski@gmail.com>
2023-01-28 01:41:57 +0100

    update files

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -18,7 +18,7 @@ void main() {
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
-a
+
     // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
